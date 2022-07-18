6092a2ac7a2230f7c175301245a5be4cfd70b048
변희진 <hjbyoun@joeunins.com>
2022-07-18 19:07:24 +0900

    initial commit

---
+++ src/Gugudan.java
@@ -0,0 +1,28 @@
+
+public class Gugudan {
+	public static void main(String args[]) {
+		// 2단
+		System.out.println("2단");
+		System.out.println(2*1);
+		System.out.println(2*2);
+		System.out.println(2*3);
+		System.out.println(2*4);
+		System.out.println(2*5);
+		System.out.println(2*6);
+		System.out.println(2*7);
+		System.out.println(2*8);
+		System.out.println(2*9);
+		// 3단
+		System.out.println("3단");
+		System.out.println(2*9);
+		System.out.println(2*9);
+		System.out.println(2*9);
+		System.out.println(2*9);
+		System.out.println(2*9);
+		System.out.println(2*9);
+		System.out.println(2*9);
+		System.out.println(2*9);
+		System.out.println(2*9);
+		
+	}
+}
